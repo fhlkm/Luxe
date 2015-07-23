c3b1b36a0247e9b0a26e13a76f805fec49318706
timfeng <hfeng11@hawk.iit.edu>
2015-07-23 10:28:25 -0500

    maps code

---
+++ MyApplication/app/src/main/java/demo/maps/com/adapter/AutocompletePlaceAdapter.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2015 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package demo.maps.com.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import demo.maps.com.myapplication.R;
+
+
+public class AutocompletePlaceAdapter extends ArrayAdapter<AutocompletePlaceAdapter.AutocompletePlace> implements Filterable {
+    private LayoutInflater mInflater;
+
+    private static final String TAG = "AutocompletePlaceAdapter";
+    /**
+     * Current results returned by this adapter.
+     */
+    private ArrayList<AutocompletePlace> mResultList;
+
+    /**
+     * Handles autocomplete requests.
+     */
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * The bounds used for Places Geo Data autocomplete API requests.
+     */
+    private LatLngBounds mBounds;
+
+    /**
+     * The autocomplete filter used to restrict queries to a specific set of place types.
+     */
+    private AutocompleteFilter mPlaceFilter;
+
+    /**
+     * Initializes with a resource for text rows and autocomplete query bounds.
+     *
+     * @see android.widget.ArrayAdapter#ArrayAdapter(android.content.Context, int)
+     */
+
+    public AutocompletePlaceAdapter(Context mContext, int resource, ArrayList<AutocompletePlace> mResultList, GoogleApiClient googleApiClient,
+                                    LatLngBounds bounds, AutocompleteFilter filter) {
+        super(mContext, resource, mResultList);
+        this.mResultList = mResultList;
+        this.mGoogleApiClient = googleApiClient;
+        this.mBounds = bounds;
+        this.mPlaceFilter = filter;
+        this.mInflater = LayoutInflater.from(mContext);
+    }
+    /**
+     * Returns the number of results received in the last autocomplete query.
+     */
+    @Override
+    public int getCount() {
+        return mResultList.size();
+    }
+
+    /**
+     * Returns an item from the last autocomplete query.
+     */
+    @Override
+    public AutocompletePlace getItem(int position) {
+        return mResultList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder mholder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.item, null);
+            mholder = new ViewHolder();
+            mholder.mName = (TextView) convertView.findViewById(R.id.mName);
+            mholder.mAddress = (TextView) convertView.findViewById(R.id.mAddress);
+            convertView.setTag(mholder);
+        } else {
+            mholder = (ViewHolder) convertView.getTag();
+        }
+        if (mResultList != null && mResultList.size() > 0) {
+            AutocompletePlace mAutoComplete = mResultList.get(position);
+            String description = mAutoComplete.toString();
+            String name = "";
+            String address = "";
+            if (description.split(",").length > 0)
+                name = description.split(",")[0];
+            if (description.split(",").length > 1)
+                address = description.split(",")[1];
+            if (description.split(",").length > 2)
+                address = address + " " + description.split(",")[2];
+            mholder.mName.setText(name);
+            mholder.mAddress.setText(address);
+        }
+
+        return convertView;
+    }
+
+
+    /**
+     * Returns the filter for the current set of autocomplete results.
+     */
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                if (constraint != null) {
+                    // Query the autocomplete API for the (constraint) search string.
+                    mResultList = getAutocomplete(constraint);
+                    if (mResultList != null) {
+                        // The API successfully returned results.
+                        results.values = mResultList;
+                        results.count = mResultList.size();
+                    }
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    // The API returned at least one result, update the data.
+                    notifyDataSetChanged();
+                } else {
+                    // The API did not return any results, invalidate the data set.
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
+        return filter;
+    }
+
+    /**
+     * Submits an autocomplete query to the Places Geo Data Autocomplete API.
+     * Results are returned as {@link AutocompletePlaceAdapter.AutocompletePlace}
+     * objects to store the Place ID and description that the API returns.
+     * Returns an empty list if no results were found.
+     * Returns null if the API client is not available or the query did not complete
+     * successfully.
+     *
+     * @param constraint Autocomplete query string
+     * @return Results from the autocomplete API or null if the query was not successful.
+     * @see com.google.android.gms.location.places.Places#GEO_DATA_API#getAutocomplete(CharSequence)
+     */
+    private ArrayList<AutocompletePlace> getAutocomplete(CharSequence constraint) {
+        if (mGoogleApiClient.isConnected()) {
+            Log.i(TAG, "Starting autocomplete query for: " + constraint);
+            PendingResult<AutocompletePredictionBuffer> results =
+                    Places.GeoDataApi .getAutocompletePredictions(mGoogleApiClient, constraint.toString(),mBounds, mPlaceFilter);
+
+            // This method should have been called off the main UI thread. Block and wait for at most 60s
+            // for a result from the API.
+            AutocompletePredictionBuffer autocompletePredictions = results.await(60, TimeUnit.SECONDS);
+
+            // Confirm that the query completed successfully, otherwise return null
+            final Status status = autocompletePredictions.getStatus();
+            if (!status.isSuccess()) {
+                Toast.makeText(getContext(), "Error contacting API: " + status.toString(),
+                        Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "Error getting autocomplete prediction API call: " + status.toString());
+                autocompletePredictions.release();
+                return null;
+            }
+
+            Log.i(TAG, "Query completed. Received " + autocompletePredictions.getCount()
+                    + " predictions.");
+
+            // Copy the results into our own data structure, because we can't hold onto the buffer.
+            // AutocompletePrediction objects encapsulate the API response (place ID and description).
+
+            Iterator<AutocompletePrediction> iterator = autocompletePredictions.iterator();
+            ArrayList resultList = new ArrayList<>(autocompletePredictions.getCount());
+            while (iterator.hasNext()) {
+                AutocompletePrediction prediction = iterator.next();
+                // Get the details of this prediction and copy it into a new PlaceAutocomplete object.
+                resultList.add(new AutocompletePlace(prediction.getPlaceId(),
+                        prediction.getDescription()));
+            }
+
+            // Release the buffer now that all data has been copied.
+            autocompletePredictions.release();
+
+            return resultList;
+        }
+        Log.e(TAG, "Google API client is not connected for autocomplete query.");
+        return null;
+    }
+
+    /**
+     * Holder for Places Geo Data Autocomplete API results.
+     */
+    public class AutocompletePlace {
+
+        public CharSequence placeId;
+        public CharSequence description;
+
+        AutocompletePlace(CharSequence placeId, CharSequence description) {
+            this.placeId = placeId;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return description.toString();
+        }
+    }
+
+
+    public class ViewHolder {
+        public TextView mName;
+        public TextView mAddress;
+
+    }
+}
---
+++ MyApplication/app/src/main/java/demo/maps/com/myapplication/MainActivity.java
@@ -0,0 +1,320 @@
+package demo.maps.com.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import demo.maps.com.adapter.AutocompletePlaceAdapter;
+
+public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+    public static final String TAG = "MainActivity";
+    public static final String PROVIDER_NAME =LocationManager.GPS_PROVIDER;
+    private boolean  is_Gps_Just_Turned_on= false;
+    private Location myLocation= null;
+    private ProgressDialog dialog= null;
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    /**
+     * GoogleApiClient wraps our service connection to Google Play Services and provides access
+     * to the user's sign in state as well as the Google's APIs.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+    private AutocompletePlaceAdapter mAdapter;
+    private Marker mapMarker;
+    private static LatLngBounds BOUNDS_CURRENT_LOCATION = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+        setUpMapIfNeeded();
+    }
+
+
+    /**
+     * Listener that handles selections from suggestions from the AutoCompleteTextView that
+     * displays Place suggestions.
+     * Gets the place id of the selected item and issues a request to the Places Geo Data API
+     * to retrieve more details about the place.
+     *
+     * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
+     * String...)
+     */
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a PlaceAutocomplete object from which we
+             read the place ID.
+              */
+            final AutocompletePlaceAdapter.AutocompletePlace item = mAdapter.getItem(position);
+            final String placeId = String.valueOf(item.placeId);
+            Log.i(TAG, "Autocomplete item selected: " + item.description);
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+              details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+            Log.i(TAG, "Called getPlaceById to get Place details for " + item.placeId);
+        }
+    };
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
+        // when we just open app, after we turn on gps, we should do localization
+        }else if(is_Gps_Just_Turned_on){
+            setUpMap();
+            is_Gps_Just_Turned_on= false;
+        }
+
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(PROVIDER_NAME, 5000, 5,locationListener);
+        // Get Current Location
+        myLocation = getLocation(locationManager);
+        if (null != myLocation) {
+            findCurrentLocation(myLocation);
+            openSearchFunction();
+        } else if(!isLocationAccess(locationManager)){
+            displayPromptForEnablingGPS(this);
+        }else{// we are getting location
+            loadingLocation();
+        }
+    }
+
+    public void findCurrentLocation(Location myLocation) {
+        if (mMap != null) {
+            // set map type
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            // Get latitude of the current location
+            double latitude = myLocation.getLatitude();
+            // Get longitude of the current location
+            double longitude = myLocation.getLongitude();
+            // Create a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+            BOUNDS_CURRENT_LOCATION = new LatLngBounds(new LatLng(latitude - 1, longitude - 1), new LatLng(latitude + 1, longitude + 1));
+            // Show the current location in Google Map
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            // Zoom in the Google Map
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+            mapMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!").snippet("Consider yourself located"));
+        }
+    }
+
+    public void openSearchFunction() {
+        // Construct a GoogleApiClient for the {@link Places#GEO_DATA_API} using AutoManage
+        // functionality, which automatically sets up the API client to handle Activity lifecycle
+        // events. If your activity does not extend FragmentActivity, make sure to call connect()
+        // and disconnect() explicitly.
+        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, 0 /* clientId */, this).addApi(Places.GEO_DATA_API).build();
+        AutoCompleteTextView mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.autocomplete_places);
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+        mAdapter = new AutocompletePlaceAdapter(this, R.layout.item, null, mGoogleApiClient, BOUNDS_CURRENT_LOCATION, null);
+        mAutocompleteView.setAdapter(mAdapter);
+    }
+
+    /**
+     * Prompt user to open GPS
+     *
+     * @param activity  current Activity
+     */
+    public void displayPromptForEnablingGPS(final Activity activity) {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        final String action = Settings.ACTION_LOCATION_SOURCE_SETTINGS;
+        final String message = "Do you want to open GPS setting?";
+        builder.setMessage(message).setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface d, int id) {
+                        activity.startActivity(new Intent(action));
+                        d.dismiss();
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface d, int id) {
+                Toast.makeText(MainActivity.this, R.string.open_location_accesss, Toast.LENGTH_SHORT).show();
+                d.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    /**
+     * Callback for results from a Places Geo Data API query that shows the first place result in
+     * the details view on screen.
+     */
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+            LatLng latLng = place.getLatLng();
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+            if (mapMarker != null) {
+                mapMarker.remove();
+            }
+            mapMarker = mMap.addMarker(new MarkerOptions().position(latLng).title(place.getName().toString()).snippet(place.getAddress().toString()));
+            Log.i(TAG, "Place details received: " + place.getName());
+            places.release();
+        }
+    };
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+        Toast.makeText(this, "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * Return current Location based on GPS,if GPS is turned off , it will return location based on Network
+     *
+     * @return Current Location
+     */
+    public Location getLocation(LocationManager locationManager ) {
+        Location location = null;
+        // getting GPS status
+        if(null != locationManager) {
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            // getting network status
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                return null;
+            } else {
+                if (isGPSEnabled) {
+                    Log.d("GPS", "GPS Enabled");
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                }
+                if (isNetworkEnabled&& location== null) {
+                    Log.d("NetWork", "NetWork Enabled");
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+            }
+        }
+        return location;
+    }
+
+    public boolean isLocationAccess(LocationManager locationManager){
+        if(null != locationManager) {
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }
+        return false;
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i(TAG, "new Location");
+            if (myLocation == null) {
+                myLocation = location;
+                if (dialog != null && dialog.isShowing()) {
+                    dialog.dismiss();
+                }
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.i(TAG,"ProviderEnable");
+            is_Gps_Just_Turned_on= true;
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    };
+
+    private void loadingLocation(){
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("loading...");
+        dialog.setIndeterminate(true);
+        dialog.setCancelable(true);
+        dialog.show();
+    }
+}
